4fa9fe88373d41a2fc6abd365ecfc456790fb9b6
Pavel Nikolov <me@pavelnikolov.net>
2017-07-03 02:07:37 +1000

    Add CNAME + Google Analytics

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -25,5 +25,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+cp CNAME public/
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git push origin gh-pages
